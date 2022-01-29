20531fd9620eea1c968a076c2046cc3b05f591a6
sasa1009 <35237920+sasa1009@users.noreply.github.com>
2022-01-30 00:54:24 +0900

    devise-token-authによる認証機能を追加 Userモデルを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
+  namespace :api do
+    namespace :v1 do
+      get 'test/index'
+
+      # mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+      #   registrations: 'api/v1/auth/registrations'
+      # }
+      mount_devise_token_auth_for 'User', at: 'auth'
+
+      namespace :auth do
+        resources :sessions, only: %i[index]
+      end
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
