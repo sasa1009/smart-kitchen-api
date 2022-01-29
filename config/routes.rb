e4d1d60552d567c9663869428be0202196b20be7
sasa1009 <35237920+sasa1009@users.noreply.github.com>
2022-01-30 03:44:03 +0900

    ユーザー登録時に名前を登録できるように修正 sessions_controllerを追加して、ログイン中のユーザーを確認できるように修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,11 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   namespace :api do
     namespace :v1 do
-      get 'test/index'
+      resources :test, only: %i[index]
 
-      # mount_devise_token_auth_for 'User', at: 'auth', controllers: {
-      #   registrations: 'api/v1/auth/registrations'
-      # }
-      mount_devise_token_auth_for 'User', at: 'auth'
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'api/v1/auth/registrations'
+      }
 
       namespace :auth do
         resources :sessions, only: %i[index]
