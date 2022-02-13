15e6f99930237a20e2df89c7f6182d2564b0de78
sasa1009 <35237920+sasa1009@users.noreply.github.com>
2022-02-14 01:32:47 +0900

    Relationshipモデルとマイグレーションファイルを追加 UserモデルとRelationshipモデルに関係を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,24 @@ class User < ActiveRecord::Base
   has_many :recipes, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :favorited_recipes, through: :favorites, source: :recipe
+
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user
+
+  def follow(other_user)
+    unless self == other_user
+      self.relationships.create!(follow_id: other_user.id)
+    end
+  end
+
+  def unfollow(other_user)
+    relationship = self.relationships.find_by(follow_id: other_user.id)
+    relationship.destroy!
+  end
+
+  def following?(other_user)
+    self.followings.include?(other_user)
+  end
 end
