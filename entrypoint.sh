0102048888c24a7c217a4798df68f43e3a8321cf
sasa1009 <35237920+sasa1009@users.noreply.github.com>
2022-05-01 05:50:56 +0900

    ターミナルをアタッチしたときにサーバーログを出力させるため、railsサーバーをpumaコマンド起動していたのをrails sコマンドで起動するように変更

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,7 +15,10 @@ set -e
 sudo service nginx start
 
 # /smart-kitchen-api/tmp/sockets/puma.sockを出力させるためにこちらのコマンドを使用する
-rm -f tmp/pids/server.pid && bundle exec puma -C config/puma.rb
+# rm -f tmp/pids/server.pid && bundle exec puma -C config/puma.rb
+
+# ターミナルにログを出力させたいのでrails s でrailsサーバーを起動する
+rm -f tmp/pids/server.pid && bundle exec rails s -p 3000 -b '0.0.0.0'
 
 # Dockerfile内のCMDを実行する
 # exec "$@"
